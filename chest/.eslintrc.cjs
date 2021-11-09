2fabfa9c71612d9eda9226ac3f38fe4115824e3f
fnbellomo <fnbellomo@gmail.com>
2021-11-08 21:26:56 -0300

    [Hotfix] config

--- chest/.eslintrc.cjs
+++ chest/.eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
     ecmaVersion: 2020,
   },
